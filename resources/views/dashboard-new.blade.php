8dd1526f0fd633d5881a319aa1235ed9ddc2b3ea
ReynaldiRP <rizkypratama471@gmail.com>
2025-10-23 11:07:14 +0700

    feat: add layouting for the apps

---
+++ resources/views/dashboard-new.blade.php
@@ -0,0 +1,10 @@
+@extends('layouts.app', ['pageSlug' => 'dashboard'])
+@section('content')
+    <div class="row">
+        <div class="col-md-12">
+            <h1>Welcome to the New Dashboard</h1>
+            <p>This is the new dashboard layout.</p>
+        </div>
+    </div>
+@endsection
+
---
+++ resources/views/layouts/navbars/sidebar.blade.php
@@ -0,0 +1,58 @@
+<div class="sidebar">
+    <div class="sidebar-wrapper">
+        <div class="logo">
+            <a href="#" class="simple-text logo-mini">MJS</a>
+            <a href="#" class="simple-text logo-normal">PT. Maju Jaya Sentosa</a>
+        </div>
+        <ul class="nav">
+            <li @if ($pageSlug == 'dashboard') class="active " @endif>
+                <a href="#">
+                    <i class="tim-icons icon-chart-pie-36"></i>
+                    <p>Dashboard</p>
+                </a>
+            </li>
+            <li>
+                <a data-toggle="collapse" href="#laravel-examples" aria-expanded="true">
+                    <i class="fab fa-laravel" ></i>
+                    <span class="nav-link-text" >{{ __('User Managements') }}</span>
+                    <b class="caret mt-1"></b>
+                </a>
+
+                <div class="collapse show" id="laravel-examples">
+                    <ul class="nav pl-4">
+                        <li @if ($pageSlug == 'profile') class="active " @endif>
+                            <a href="#">
+                                <i class="tim-icons icon-single-02"></i>
+                                <p>{{ __('User Roles') }}</p>
+                            </a>
+                        </li>
+                        <li @if ($pageSlug == 'users') class="active " @endif>
+                            <a href="#">
+                                <i class="tim-icons icon-bullet-list-67"></i>
+                                <p>{{ __('User Profile') }}</p>
+                            </a>
+                        </li>
+                    </ul>
+                </div>
+            </li>
+            <li @if ($pageSlug == 'icons') class="active " @endif>
+                <a href="#">
+                    <i class="tim-icons icon-atom"></i>
+                    <p>Product Type</p>
+                </a>
+            </li>
+            <li @if ($pageSlug == 'maps') class="active " @endif>
+                <a href="#">
+                    <i class="tim-icons icon-pin"></i>
+                    <p>Product</p>
+                </a>
+            </li>
+            <li @if ($pageSlug == 'notifications') class="active " @endif>
+                <a href="#">
+                    <i class="tim-icons icon-bell-55"></i>
+                    <p>Receipt</p>
+                </a>
+            </li>
+        </ul>
+    </div>
+</div>
